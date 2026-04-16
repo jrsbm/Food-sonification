2a240dea6ad31bd0258342d0e07890fe907c6ffd
jrsbm <joris.boom@xs4all.nl>
2026-04-16 12:45:27 +0100

    new uv + marimo + polars

---
+++ uv-sound/workshop.py
@@ -0,0 +1,17 @@
+import marimo
+
+__generated_with = "0.23.1"
+app = marimo.App(width="medium")
+
+
+app._unparsable_cell(
+    r"""
+    import marimo as mo
+    import 
+    """,
+    name="_"
+)
+
+
+if __name__ == "__main__":
+    app.run()
